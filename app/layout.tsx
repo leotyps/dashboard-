cfb9daee9f9d32ae3275b631affcabab04116e7d
Valzyy <jkt48connect@gmail.com>
2025-04-19 16:01:45 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { Provider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Stack Template",
-  description: "A Multi-tenant Next.js Starter Template",
+  title: "JKT48Connect - ZENOVA",
+  description: "a restapi which provides jkt48 features and also other features",
 };
 
 export default function RootLayout({
